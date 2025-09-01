0b4f24b9b8b4cb73ff2b082cf1f57a2e1b73ea5b
knighthat <git@knighthat.me>
2025-09-01 16:27:11 -0500

    fully integrate DownloadHelper to MyDownloadHelper

--- composeApp/src/androidMain/kotlin/app/kreate/android/themed/rimusic/component/song/SongItem.kt
+++ composeApp/src/androidMain/kotlin/app/kreate/android/themed/rimusic/component/song/SongItem.kt
@@ -177,11 +177,10 @@ object SongItem: Visual() {
     @UnstableApi
     @Composable
     private fun <T> CacheAndDownloadIcon(
-        context: Context,
         songId: String,
         song: T,
         values: Values,
-        handler: (Context, T, Boolean) -> Unit,
+        handler: (T, Boolean) -> Unit,
         modifier: Modifier = Modifier,
         onClick: () -> Unit = {}
     ) {
@@ -211,7 +210,7 @@ object SongItem: Visual() {
                                        formatTable.deleteBySongId( songId )
                                    }
 
-                                   handler( context, song, true )
+                                   handler( song, true )
                                }
         )
     }
@@ -219,24 +218,12 @@ object SongItem: Visual() {
     @OptIn(UnstableApi::class)
     @Composable
     fun CacheAndDownloadIcon(
-        context: Context,
         song: Song,
         values: Values,
         modifier: Modifier = Modifier,
         onClick: () -> Unit = {}
     ) =
-        CacheAndDownloadIcon( context, song.id, song, values, MyDownloadHelper::handleDownload , modifier, onClick )
-
-    @OptIn(UnstableApi::class)
-    @Composable
-    fun CacheAndDownloadIcon(
-        context: Context,
-        mediaItem: MediaItem,
-        values: Values,
-        modifier: Modifier = Modifier,
-        onClick: () -> Unit = {}
-    ) =
-        CacheAndDownloadIcon( context, mediaItem.mediaId, mediaItem, values, MyDownloadHelper::handleDownload , modifier, onClick )
+        CacheAndDownloadIcon( song.id, song, values, MyDownloadHelper::handleDownload , modifier, onClick )
 
     /**
      * Display badges such as "playlist", "explicit", etc.
@@ -484,7 +471,7 @@ object SongItem: Visual() {
                                        // it to the bottom for better view
                                        .align( Alignment.Bottom )
                 )
-                CacheAndDownloadIcon( context, song, values ) {
+                CacheAndDownloadIcon( song, values ) {
                     binder.cache.removeResource( song.id )
                 }
             },
