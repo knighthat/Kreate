402d83f0e23b8e191cebccae6b23d5e3eaf398d0
knighthat <git@knighthat.me>
2025-09-15 00:28:59 -0500

    move fields into respective locations

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/service/modern/PlayerServiceModern.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/service/modern/PlayerServiceModern.kt
@@ -150,14 +150,14 @@ class PlayerServiceModern:
     PlaybackStatsListener.Callback,
     SharedPreferences.OnSharedPreferenceChangeListener
 {
+    @Inject
+    lateinit var player: ExoPlayer
 
     @Inject
     @Named("cache")
     lateinit var cache: Cache
     @Inject
     lateinit var downloadHelper: DownloadHelper
-    @Inject
-    lateinit var player: ExoPlayer
 
     @RequiresApi(Build.VERSION_CODES.M)
     private val discord: Discord = Discord(this)
@@ -179,8 +179,6 @@ class PlayerServiceModern:
     private lateinit var downloadListener: DownloadManager.Listener
 
     private var binder = Binder()
-    private var bassBoost: BassBoost? = null
-    private var reverbPreset: PresetReverb? = null
 
     lateinit var audioQualityFormat: AudioQualityFormat
     lateinit var sleepTimer: SleepTimer
@@ -544,6 +542,8 @@ class PlayerServiceModern:
         }
     }
 
+    private var bassBoost: BassBoost? = null
+
     private fun maybeBassBoost() {
         if ( !Preferences.AUDIO_BASS_BOOSTED.value ) {
             runCatching {
@@ -567,6 +567,8 @@ class PlayerServiceModern:
             Toaster.e( "Can't enable bass boost" )
         }
     }
+
+    private var reverbPreset: PresetReverb? = null
 
     private fun maybeReverb() {
         val presetType by Preferences.AUDIO_REVERB_PRESET
