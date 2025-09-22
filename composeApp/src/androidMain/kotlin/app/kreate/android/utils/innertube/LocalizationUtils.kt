66a09c6bbfe5e509b52713888788e0df656cf4e7
knighthat <git@knighthat.me>
2025-09-21 19:11:26 -0500

    use getter instead of derived state

--- composeApp/src/androidMain/kotlin/app/kreate/android/utils/innertube/LocalizationUtils.kt
+++ composeApp/src/androidMain/kotlin/app/kreate/android/utils/innertube/LocalizationUtils.kt
@@ -1,8 +1,6 @@
 package app.kreate.android.utils.innertube
 
 import android.telephony.TelephonyManager
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
 import androidx.core.content.getSystemService
 import app.kreate.android.Preferences
 import it.fast4x.rimusic.appContext
@@ -11,13 +9,12 @@ import me.knighthat.innertube.request.Localization
 import java.util.Locale
 
 
-val CURRENT_LOCALE: Localization by derivedStateOf {
-    Localization(HOST_LANGUAGE, GEO_LOCATION)
-}
+val CURRENT_LOCALE: Localization
+    get() = Localization(HOST_LANGUAGE, GEO_LOCATION)
 
 // hl
-val HOST_LANGUAGE: String by derivedStateOf {
-    when ( Preferences.APP_LANGUAGE.value ) {
+val HOST_LANGUAGE: String
+    get() = when ( Preferences.APP_LANGUAGE.value ) {
         Languages.System ->
             try {
                 enumValueOf<Languages>(Locale.getDefault().language).code
@@ -27,16 +24,15 @@ val HOST_LANGUAGE: String by derivedStateOf {
 
         else -> Preferences.APP_LANGUAGE.value.code
     }
-}
 
 // gl
-val GEO_LOCATION: String by derivedStateOf {
-    var countryCode = Preferences.APP_REGION.value
-    if( countryCode.isBlank() || countryCode !in Locale.getISOCountries() )
-        countryCode = getSystemCountryCode()
-
-    countryCode
-}
+val GEO_LOCATION: String
+    get() = Preferences.APP_REGION.value.let {
+        if( it.isBlank() || it !in Locale.getISOCountries() )
+            getSystemCountryCode()
+        else
+            it
+    }
 
 fun getSystemCountryCode(): String {
     var countryCode = Locale.getDefault().country
