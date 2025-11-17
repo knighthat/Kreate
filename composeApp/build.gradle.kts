53e1322be810b8508deffdb4abfaf2e894a1e68d
12fab4 <mailfuerfabian@web.de>
2025-11-17 18:50:01 +0200

    fixup! Add option to reset the live wallpaper after a configurable timeout
    Revert debug signing accidentally enabled for debug purposes

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -232,7 +232,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug")
         }
         create( "uncompressed" ) {
             // App's properties
