f98f397686fa654ff2f6c2830f858480cb2614f0
knighthat <git@knighthat.me>
2025-11-26 13:45:11 -0600

    remove top bar after splashscreen

--- composeApp/src/androidMain/kotlin/me/knighthat/kreate/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/me/knighthat/kreate/MainActivity.kt
@@ -3,7 +3,6 @@ package me.knighthat.kreate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -15,8 +14,6 @@ import kotlinx.coroutines.launch
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
-
         // Install the splash screen *before* calling super.onCreate()
         val splashScreen = installSplashScreen()
 
