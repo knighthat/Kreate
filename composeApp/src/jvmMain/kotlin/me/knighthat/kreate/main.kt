7db8375380ab9c6adf1bdbb362cf9e2505218648
knighthat <git@knighthat.me>
2025-11-25 23:33:00 -0600

    add app_name to commonMain strings

--- composeApp/src/jvmMain/kotlin/me/knighthat/kreate/main.kt
+++ composeApp/src/jvmMain/kotlin/me/knighthat/kreate/main.kt
@@ -2,11 +2,14 @@ package me.knighthat.kreate
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kreate.composeapp.generated.resources.Res
+import kreate.composeapp.generated.resources.app_name
+import org.jetbrains.compose.resources.stringResource
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "Kreate",
+        title = stringResource( Res.string.app_name ),
     ) {
         App()
     }
