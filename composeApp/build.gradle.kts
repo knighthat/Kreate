bfc2babf0c59d99bd96a260f0a9a51968c8a30a8
knighthat <git@knighthat.me>
2025-11-25 23:28:39 -0600

    add: theme adaptive splashscreen

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -56,6 +56,7 @@ kotlin {
 
             implementation( libs.androidx.appcompat )
             implementation( libs.androidx.core.ktx )
+            implementation( libs.androidx.splashscreen )
         }
         jvmMain.dependencies {
             implementation( compose.desktop.currentOs )
