dfb307bb7918dd2fed01a6fb991e1a070707b2c7
12fab4 <mailfuerfabian@web.de>
2025-12-06 14:32:43 +0200

    fixup! Add option to reset the live wallpaper after a configurable timeout
    change reset duration to a long pref with a slider

--- composeApp/src/androidMain/kotlin/app/kreate/android/themed/common/screens/settings/AppearanceSettings.kt
+++ composeApp/src/androidMain/kotlin/app/kreate/android/themed/common/screens/settings/AppearanceSettings.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -31,13 +32,14 @@ import it.fast4x.rimusic.utils.isAtLeastAndroid7
 import it.fast4x.rimusic.utils.isLandscape
 import me.knighthat.utils.Toaster
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppearanceSettings( paddingValues: PaddingValues ) {
+fun AppearanceSettings(paddingValues: PaddingValues) {
     val scrollState = rememberLazyListState()
     val isLandscapeMode = isLandscape
 
     val search = remember {
-        SettingEntrySearch( scrollState, R.string.player_appearance, R.drawable.color_palette )
+        SettingEntrySearch(scrollState, R.string.player_appearance, R.drawable.color_palette)
     }
 
     Column(
@@ -95,12 +97,21 @@ fun AppearanceSettings( paddingValues: PaddingValues ) {
                         )
                 }
             }
-            entry(search, R.string.settings_entry_live_wallpaper_reset_duration){
-                SettingComponents.EnumEntry(
+            entry(search, R.string.settings_entry_live_wallpaper_reset_duration) {
+                SettingComponents.SliderEntry(
                     preference = Preferences.LIVE_WALLPAPER_RESET_DURATION,
                     title = stringResource(R.string.settings_entry_live_wallpaper_reset_duration),
                     subtitle = stringResource(R.string.settings_description_live_wallpaper_reset_duration),
-                            action = SettingComponents.Action.RESTART_PLAYER_SERVICE
+                    constraints = "",
+                    valueRange = -1f..60000f,
+                    steps = 61,
+                    onValueChangeFinished = { pref, value -> pref.value = value.toLong() },
+                    onTextDisplay = {
+                        when (it) {
+                            -1f -> stringResource(R.string.disabled)
+                            else -> (it / 1000).toString()
+                        }
+                    }
                 )
             }
         }
