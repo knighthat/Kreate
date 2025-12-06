5d9f13d3422d7ca458b9bdb16c50c40de02a317b
Yehooda Romem <yrrad8@gmail.com>
2025-12-06 22:35:53 +0200

    added `https://yt3.googleusercontent.com` to urls with dynamic size

--- composeApp/src/commonMain/kotlin/app/kreate/util/ThumbnailUtils.kt
+++ composeApp/src/commonMain/kotlin/app/kreate/util/ThumbnailUtils.kt
@@ -8,6 +8,7 @@ fun String?.thumbnail(size: Int): String? {
     return when {
         this?.startsWith("https://lh3.googleusercontent.com") == true -> "$this-w$size-h$size"
         this?.startsWith("https://yt3.ggpht.com") == true -> "$this-w$size-h$size-s$size"
+        this?.startsWith("https://yt3.googleusercontent.com") == true -> "$this-w$size-h$size-s$size"
         else -> this
     }
 }
