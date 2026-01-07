5678ead4a5e175fc3d7868f095dab02e9245b7a6
knighthat <git@knighthat.me>
2026-01-06 23:11:29 -0600

    bump version from 1.9.0 to 1.9.1 (127)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -9,7 +9,7 @@ import java.text.SimpleDateFormat
 import java.util.Date
 
 val APP_NAME = "Kreate"
-val VERSION_CODE = 126
+val VERSION_CODE = 127
 
 private fun String.sha256(): String {
     val digest = MessageDigest.getInstance( "SHA-256" )
@@ -331,7 +331,7 @@ android {
                 signingConfig = signingConfigs.getByName( "production" )
 
             // App's properties
-            versionName = "1.9.0"
+            versionName = "1.9.1"
             manifestPlaceholders["appName"] = APP_NAME
             versionCode = VERSION_CODE
         }
