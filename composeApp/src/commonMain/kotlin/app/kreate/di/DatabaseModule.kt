789b5d7f87c694b544e2478c9414dde7f2249849
knighthat <git@knighthat.me>
2026-06-27 13:29:02 -0500

    clean ghost maps and enable PRAGMA configs on db open

--- composeApp/src/commonMain/kotlin/app/kreate/di/DatabaseModule.kt
+++ composeApp/src/commonMain/kotlin/app/kreate/di/DatabaseModule.kt
@@ -1,7 +1,9 @@
 package app.kreate.di
 
 import androidx.room.RoomDatabase
+import androidx.sqlite.SQLiteConnection
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import androidx.sqlite.execSQL
 import app.kreate.database.AppDatabase
 import app.kreate.database.migration.From10To11Migration
 import app.kreate.database.migration.From14To15Migration
@@ -17,6 +19,7 @@ import app.kreate.database.migration.From30To31Migration
 import app.kreate.database.migration.From34To35Migration
 import app.kreate.database.migration.From35To36Migration
 import app.kreate.database.migration.From8To9Migration
+import co.touchlab.kermit.Logger
 import kotlinx.coroutines.Dispatchers
 import org.koin.core.scope.Scope
 import org.koin.dsl.module
@@ -29,6 +32,7 @@ val databaseModule = module {
         getDatabaseBuilder( this@single )
             .setDriver( BundledSQLiteDriver() )
             .setQueryCoroutineContext( Dispatchers.IO )
+            .addCallback( DatabaseInitCallback() )
             .addMigrations(
                 From8To9Migration(),
                 From10To11Migration(),
@@ -46,5 +50,65 @@ val databaseModule = module {
                 From35To36Migration()
             )
             .build()
+    }
+}
+
+private class DatabaseInitCallback : RoomDatabase.Callback() {
+
+    override fun onOpen( connection: SQLiteConnection ) {
+        super.onOpen( connection )
+
+        // Clear ghost maps on open
+        try {
+            connection.execSQL("""
+                DELETE FROM formats 
+                WHERE song_id NOT IN (SELECT id FROM songs);
+            """.trimIndent())
+                connection.execSQL("""
+                DELETE FROM lyrics 
+                WHERE song_id NOT IN (SELECT id FROM songs);
+            """.trimIndent())
+                connection.execSQL("""
+                DELETE FROM persistent_queue 
+                WHERE song_id NOT IN (SELECT id FROM songs);
+            """.trimIndent())
+                connection.execSQL("""
+                DELETE FROM playback_history 
+                WHERE song_id NOT IN (SELECT id FROM songs);
+            """.trimIndent())
+                connection.execSQL("""
+                DELETE FROM song_album_map 
+                WHERE song_id NOT IN (SELECT id FROM songs)
+                OR album_id NOT IN (SELECT id FROM albums);
+            """.trimIndent())
+                connection.execSQL("""
+                DELETE FROM song_artist_map 
+                WHERE song_id NOT IN (SELECT id FROM songs)
+                OR artist_id NOT IN (SELECT id FROM artists);
+            """.trimIndent())
+                connection.execSQL("""
+                DELETE FROM song_playlist_map 
+                WHERE song_id NOT IN (SELECT id FROM songs)
+                OR playlist_id NOT IN (SELECT id FROM playlists);
+            """.trimIndent())
+        } catch( err: Exception ) {
+            Logger.e( err, "DatabaseInitCallback" ) { "failed to clear ghost maps" }
+        }
+
+
+        // Enables foreign key constraint enforcement.
+        // Enforces @ForeignKey annotations, operations such as
+        // onUpdate and onDelete will be enacted with this enabled.
+        connection.execSQL( "PRAGMA foreign_keys = ON;" )
+        // WAL allows simultaneous reads and writes.
+        // It significantly boosts performance for concurrent database operations.
+        // SQLite driver is included with build so this feature is safe to use.
+        connection.execSQL( "PRAGMA journal_mode = WAL;" )
+        // NORMAL is much faster and completely safe when combined with WAL mode,
+        // as it still guarantees database integrity in the event of an application crash.
+        connection.execSQL( "PRAGMA synchronous = NORMAL;" )
+        // Sets a timeout (in milliseconds) for how long SQLite will wait for
+        // a locked table to clear before throwing a [SQLiteDatabaseLockedException].
+        connection.execSQL( "PRAGMA busy_timeout = 3000;" )
     }
 }
